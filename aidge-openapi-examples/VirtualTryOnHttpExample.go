468d8af85d093c5a72a152c54492396f09e6a562
zhishan <pengcheng.bpc@alibaba-inc.com>
2025-03-14 11:34:28 +0800

    add comments for VTO

--- aidge-openapi-examples/VirtualTryOnHttpExample.go
+++ aidge-openapi-examples/VirtualTryOnHttpExample.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -25,8 +25,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
-	"time"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -37,22 +37,25 @@ func main() {
 	fmt.Println(accessKeySecret)
 
 	/* "api.aidc-ai.com" for api purchased on global site
-    * 中文站购买的API请使用"cn-api.aidc-ai.com" (for api purchased on chinese site)
-    */
-    apiDomain := "your api domain"
+	 * 中文站购买的API请使用"cn-api.aidc-ai.com" (for api purchased on chinese site)
+	 */
+	apiDomain := "your api domain"
 
-    /**
-     * We offer trial quota to help you familiarize and test how to use the Aidge API in your account
-     * To use trial quota, please set useTrialResource to true
-     * If you set useTrialResource to false before you purchase the API
-     * You will receive "Sorry, your calling resources have been exhausted........"
-     * 我们为您的账号提供一定数量的免费试用额度可以试用任何API。请将useTrialResource设置为true用于试用。
-     * 如设置为false，且您未购买该API，将会收到"Sorry, your calling resources have been exhausted........."的错误提示
-     */
-    useTrialResource := false/true
+	/**
+	 * We offer trial quota to help you familiarize and test how to use the Aidge API in your account
+	 * To use trial quota, please set useTrialResource to true
+	 * If you set useTrialResource to false before you purchase the API
+	 * You will receive "Sorry, your calling resources have been exhausted........"
+	 * 我们为您的账号提供一定数量的免费试用额度可以试用任何API。请将useTrialResource设置为true用于试用。
+	 * 如设置为false，且您未购买该API，将会收到"Sorry, your calling resources have been exhausted........."的错误提示
+	 */
+	useTrialResource := false / true
 
 	// Call virtual try on submit
 	apiName := "/ai/virtual/tryon"
+
+	// URL of the clothing image should be accessible from the public network.
+	// The resolution should be greater than 500x500 pixels and up to a maximum of 3000x3000 pixels
 	submitRequest := `{"requestParams":"[{\"clothesList\":[{\"imageUrl\":\"https://ae-pic-a1.aliexpress-media.com/kf/H7588ee37b7674fea814b55f2f516fda1z.jpg\",\"type\":\"tops\"}],\"model\":{\"base\":\"General\",\"gender\":\"female\",\"style\":\"universal_1\",\"body\":\"slim\"},\"viewType\":\"mixed\",\"inputQualityDetect\":0,\"generateCount\":4}]"}`
 	submitResult, err := invokeApi(accessKeyName, accessKeySecret, apiName, apiDomain, submitRequest, useTrialResource)
 	if err != nil {
@@ -120,10 +123,10 @@ func invokeApi(accessKeyName, accessKeySecret, apiName, apiDomain, data string, 
 		"Content-Type": "application/json",
 	}
 
-    // Add "x-iop-trial": "true" for trial
+	// Add "x-iop-trial": "true" for trial
 	if useTrialResource {
-        headers["x-iop-trial"] = "true"
-    }
+		headers["x-iop-trial"] = "true"
+	}
 
 	// Do HTTP POST request
 	response, err := makeRequest("POST", finalURL, data, headers)
